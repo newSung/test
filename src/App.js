c11a90cb99c43b3dba105ec64f6358c4e05f3be7
newSung <gho0612@gmail.com>
2023-08-21 17:17:05 +0900

    zz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const url = 'https://port-0-servertest-619z9h2lllkaic66.sel3.cloudtype.app/api/test'
+
+const onSubmitHandler = (event) => {
+  console.log('this is test')
+  axios.get(url).then(response => {
+    console.log(response.data.text)
+  })
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <button type="" onClick={onSubmitHandler}>click</button>
     </div>
   );
 }
